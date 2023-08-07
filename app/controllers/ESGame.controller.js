a6f98dfe2fed107551e323a47a950fcf739f8cbc
temeculous <zacerious@gmail.com>
2023-08-07 13:59:31 -0400

    controller stubbed

---
+++ app/controllers/ESGame.controller.js
@@ -0,0 +1,21 @@
+const db = require("../models");
+const ESGame = db.esgame;
+const Op = db.Sequelize.Op;
+
+//Create new game instance
+exports.create = (req, res) => {};
+
+//Retrieve all games
+exports.getAll = (req, res) => {};
+
+//Retrieve single game by id
+exports.getOne = (req, res) => {};
+
+//Update single game by id
+exports.update = (req, res) => {};
+
+//Delete single game by id
+exports.deleteOne = (req, res) => {};
+
+//Delete all games from db
+exports.deleteAll = (req, res) => {};
