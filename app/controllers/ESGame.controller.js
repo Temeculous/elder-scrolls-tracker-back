f35707ff56a9c7ec7cab115a9d6fc826abe690d0
temeculous <zacerious@gmail.com>
2023-08-07 14:06:43 -0400

    create controller built

--- app/controllers/ESGame.controller.js
+++ app/controllers/ESGame.controller.js
@@ -3,7 +3,31 @@ const ESGame = db.esgame;
 const Op = db.Sequelize.Op;
 
 //Create new game instance
-exports.create = (req, res) => {};
+exports.create = (req, res) => {
+  if (!req.body.title) {
+    res.status(400).send({ message: "Title is required" });
+    return;
+  }
+  const game = {
+    title: req.body.title,
+    popularity: req.body.popularity,
+    releaseYear: req.body.releaseYear,
+    completionHours: req.body.completionHours,
+    imgURL: req.body.imgURL,
+  };
+
+  ESGame.create(game)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res
+        .statys(500)
+        .send({
+          message: err.message || "An error occured while creating new game!",
+        });
+    });
+};
 
 //Retrieve all games
 exports.getAll = (req, res) => {};
