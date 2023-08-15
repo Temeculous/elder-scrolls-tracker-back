470142618b8612015e2d8ff306800126910238c6
temeculous <zacerious@gmail.com>
2023-08-15 13:36:27 -0400

    trailing comma

--- init.sql
+++ init.sql
@@ -3,7 +3,7 @@ CREATE TABLE esgames (
     title VARCHAR(255) NOT NULL,
     popularity VARCHAR(255) NOT NULL,
     releaseYear INTEGER NOT NULL,
-    completionHours INTEGER NOT NULL,
+    completionHours INTEGER NOT NULL
 );
 
 INSERT INTO esgames (title,popularity,releaseYear,completionHours)
