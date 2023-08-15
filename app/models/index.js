6999af571ecf9c243595ae5526fe9f18e38451f5
temeculous <zacerious@gmail.com>
2023-08-15 17:26:04 -0400

    postgres heroku sequelize

--- app/models/index.js
+++ app/models/index.js
@@ -1,18 +1,36 @@
 const dbConfig = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  operatorsAliases: false,
-
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
   },
 });
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("succesful connection");
+  })
+  .catch((err) => {
+    console.log("Unable to connect to database:" + err);
+  });
+// incase disaster use this for local :)
+// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+//   host: dbConfig.HOST,
+//   dialect: dbConfig.dialect,
+//   operatorsAliases: false,
+
+//   pool: {
+//     max: dbConfig.pool.max,
+//     min: dbConfig.pool.min,
+//     acquire: dbConfig.pool.acquire,
+//     idle: dbConfig.pool.idle,
+//   },
+// });
 
 const db = {};
 
