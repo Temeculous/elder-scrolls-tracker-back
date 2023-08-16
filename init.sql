5460ed8ae4e2e94b71003f3725911dfaa1b472a0
temeculous <zacerious@gmail.com>
2023-08-15 21:01:32 -0400

    sql corrections, almost there

--- init.sql
+++ init.sql
@@ -4,9 +4,8 @@ CREATE TABLE esgames (
     "popularity" VARCHAR(255) NOT NULL,
     "releaseYear" INTEGER NOT NULL,
     "completionHours" INTEGER NOT NULL,
-    "createdAt" TIMESTAMP NOT NULL,
-    "updatedAt" TIMESTAMP NOT NULL
+    "createdAt" DATE NOT NULL DEFAULT CURRENT_DATE,
+    "updatedAt" DATE
 );
-
-INSERT INTO esgames (title,popularity,releaseYear,completionHours)
-VALUES ("Elder Scrolls 1: Arena", "Not very popular", 1994, 44);
+INSERT INTO esgames ("title","popularity","releaseYear","completionHours")
+VALUES ('Elder Scrolls 1: Arena', 'Not very popular', 1994, 44);
